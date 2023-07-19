e8b37ad2577974188a8c2366a80f016a9c47fc20
arihanv <arihanvaranasi@gmail.com>
2023-07-19 14:59:39 -0500

    feat: Make NavBar Responsive with Small Menu Bar

---
+++ components/smallMenu.tsx
@@ -0,0 +1,38 @@
+"use client"
+import React from "react"
+import { Menu } from "lucide-react"
+
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+
+type Props = {}
+
+export default function SmallMenu({}: Props) {
+  return (
+    <Menubar>
+      <MenubarMenu>
+        <MenubarTrigger>
+          {" "}
+          <Menu />
+        </MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>New Window</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Share</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem>Print</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  )
+}
